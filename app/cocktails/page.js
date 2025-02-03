df122aef7018f00211fba48ce35f183d1ab4a3dc
iCabreraa <i.cabrera.hidalg@gmail.com>
2025-02-03 10:10:58 +0100

    Primer commit

---
+++ app/cocktails/page.js
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Cosmococktails</h1>
+      <ul>
+        <li>Margherita</li>
+        <li>Tequila Sunrise</li>
+        <li>Mojito</li>
+      </ul>
+    </div>
+  );
+}
--- app/cocktails/pinacolada/page.js
+++ app/cocktails/pinacolada/page.js
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <h1>Cosmococktails</h1>;
+  return <h1>Pina Colada</h1>;
 }
